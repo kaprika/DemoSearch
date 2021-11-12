6397e9dec474a90461cb6717e1d7b8794fa69e42
Elena Petrevich <elysiuk@gmail.com>
2021-11-12 12:26:58 +0300

    added message "no records found"

--- force-app/main/default/lwc/demo/demo.js
+++ force-app/main/default/lwc/demo/demo.js
@@ -5,26 +5,33 @@ export default class Demo extends LightningElement {
     @track fields = ['Name'];
     sObjectSelected = false;
     sObjectApiName;
+    labelName;
     whereConditions = '';
     orderByAsc = 'Name';
     orderByDesc = '';
     limitCondition = 3;
     placeholder = 'Search...';
+    searchKey = '';
+    //?
     @track fieldsNames = [];
 
     handleSelection(event) {
         this.sObjectSelected = true;
         this.sObjectApiName = event.detail;
+        if (this.sObjectApiName.includes('__c')) {
+            let object = this.sObjectApiName.substring(0, this.sObjectApiName.length - 3);
+            this.labelName = object.replaceAll('_', ' ');
+        } else {
+            this.labelName = this.sObjectApiName;
+        }
+        this.labelName = this.formatString(this.labelName);
         this.fieldsNames = [];
         getSObjectTypeFields({
             sObjectName: this.sObjectApiName
         })
             .then(result => {
-                let i = 0;
                 for (const fieldName of result) {
-                    i++;
                     this.fieldsNames = [... this.fieldsNames, fieldName];
-                    if (i === 6) break;
                 }
                 console.log('fields : ', this.fieldsNames);
             })
@@ -33,5 +40,13 @@ export default class Demo extends LightningElement {
             })
             .finally(() => {
             });
+    }
+
+    formatString(string) {
+        var sentence = string.toLowerCase().split(' ');
+        for (var i = 0; i < sentence.length; i++) {
+            sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+        }
+        return sentence;
     }
 }
--- force-app/main/default/lwc/objectLookup/objectLookup.js
+++ force-app/main/default/lwc/objectLookup/objectLookup.js
@@ -12,37 +12,13 @@ export default class ObjectLookup extends LightningElement {
     @api orderByAsc;
     @api orderByDesc;
     @api limitCondition;
-    resultMessage = false;
-    searchKey = '';
+    @api searchKey = '';
 
-    field;
-    field1;
-    field2;
-
-    connectedCallback() {
-        if (this.objectName.includes('__c')) {
-            let object = this.objectName.substring(0, this.objectName.length - 3);
-            this.labelName = object.replaceAll('_', ' ');
-        } else {
-            this.labelName = this.objectName;
-        }
-        this.labelName = this.formatString(this.labelName);
-
-        let fieldList = this.fields;
-        if (fieldList.length == 1) {
-            this.field = fieldList[0].trim();
-        }
-        else if (fieldList.length > 1) {
-            this.field = fieldList[0].trim();
-            this.field1 = fieldList[1].trim();
-        }
-        if (fieldList.length > 2) {
-            this.field2 = fieldList[2].trim();
-        }
+    get showResultMessage() {
+        return (this.objectList.length == 0 && this.searchKey.length > 1)
     }
 
     handleKeyChange(event) {
-        this.resultMessage = false;
         this.searchKey = event.target.value;
         window.clearTimeout(this.delayTimeout);
         this.delayTimeout = setTimeout(() => {
@@ -57,21 +33,7 @@ export default class ObjectLookup extends LightningElement {
                     limitCondition: this.limitCondition
                 })
                     .then(result => {
-                        if (result.length == 0) {
-                            this.resultMessage = true;
-                        }
-                        let stringResult = JSON.stringify(result);
-                        let allResult = JSON.parse(stringResult);
-                        allResult.forEach(record => {
-                            record.FIELD1 = record[this.field];
-                            record.FIELD2 = record[this.field1];
-                            if (this.field2) {
-                                record.FIELD3 = record[this.field2];
-                            } else {
-                                record.FIELD3 = '';
-                            }
-                        });
-                        this.objectList = allResult;
+                        this.objectList = result;
                     })
                     .catch(error => {
                         console.error('Error:', error);
@@ -83,13 +45,5 @@ export default class ObjectLookup extends LightningElement {
                 this.objectList = [];
             }
         }, DELAY);
-    }
-
-    formatString(string) {
-        var sentence = string.toLowerCase().split(' ');
-        for (var i = 0; i < sentence.length; i++) {
-            sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
-        }
-        return sentence;
     }
 }
